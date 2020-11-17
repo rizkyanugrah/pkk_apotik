7e7b553709e9cabb18fa9687a82914efa6ab0367
mrizkimaulidan <mrizkimaulidan21@gmail.com>
2020-11-17 19:04:20 +0800

    change message alert to indonesia

--- resources/views/admin/obat/index.blade.php
+++ resources/views/admin/obat/index.blade.php
@@ -70,13 +70,14 @@
         let id = $(this).data('id');
 
         Swal.fire({
-            title: 'Are you sure?',
-            text: "You won't be able to revert this!",
+            title: 'Hapus?',
+            text: "Data tidak akan bisa dikembalikan!",
             icon: 'warning',
             showCancelButton: true,
             confirmButtonColor: '#3085d6',
             cancelButtonColor: '#d33',
-            confirmButtonText: 'Yes, delete it!'
+            confirmButtonText: 'Ya',
+            cancelButtonText: 'Tidak'
         }).then((result) => {
             if (result.isConfirmed) {
                 $.ajaxSetup({
