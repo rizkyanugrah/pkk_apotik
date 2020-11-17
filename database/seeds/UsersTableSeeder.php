fc76526b736c84e43ea077720db6e6e64d10af1c
mrizkimaulidan <mrizkimaulidan21@gmail.com>
2020-11-17 11:35:46 +0800

    login feature (seeders, migrations, etc)

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'Admin',
+            'role_id' => 1,
+            'email' => 'admin@mail.com',
+            'password' => bcrypt('secret')
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'Apoteker',
+            'role_id' => 2,
+            'email' => 'apoteker@mail.com',
+            'password' => bcrypt('secret')
+        ]);
+
+        factory(User::class, 15)->make();
+    }
+}
