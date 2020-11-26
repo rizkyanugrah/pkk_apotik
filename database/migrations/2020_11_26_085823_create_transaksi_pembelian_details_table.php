3a68498d74dbc48a17d0fc782699b78fd1fe5870
MirrorBottle <setiawanbayu66152@gmail.com>
2020-11-26 17:02:24 +0800

    start transaksi pembelian

---
+++ database/migrations/2020_11_26_085823_create_transaksi_pembelian_details_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTransaksiPembelianDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transaksi_pembelian_details', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('transaksi_pembelian_id')->unsigned();
+            $table->integer('obat_id')->unsigned();
+            $table->bigInteger('total_obat');
+            $table->string('sub_total');
+            $table->timestamps();
+            $table->foreign('transaksi_pembelian_id')->references('id')->on('transaksi_pembelians')->onDelete('cascade');
+            $table->foreign('obat_id')->references('id')->on('obats')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transaksi_pembelian_details');
+    }
+}
